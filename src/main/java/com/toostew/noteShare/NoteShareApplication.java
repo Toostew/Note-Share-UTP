65e2a7f31396837ff917271ee2f0b671bfa416aa
Toostew <tookaystewie@gmail.com>
2025-12-30 21:44:57 +0800

    initial commit

---
+++ src/main/java/com/toostew/noteShare/NoteShareApplication.java
@@ -0,0 +1,13 @@
+package com.toostew.noteShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toostew/noteShare/NoteShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toostew.noteShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
