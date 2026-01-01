e82ad3c3c7de6c359e1193e254414f7fcfb3b563
Toostew <tookaystewie@gmail.com>
2026-01-01 19:09:42 +0800

    testing Cloudflare R2 getObject query

--- src/main/java/com/toostew/noteShare/controller/PageController.java
+++ src/main/java/com/toostew/noteShare/controller/PageController.java
@@ -1,6 +1,12 @@
 package com.toostew.noteShare.controller;
 
 
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +14,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.Response;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 
 @Controller
 @RequestMapping("/page")
 public class PageController {
     //front facing api
+
+    private S3Client s3client;
+
+    public PageController(S3Client s3client){
+        this.s3client = s3client;
+    }
 
     @GetMapping("/test")
     public String test(Model model){
@@ -25,5 +43,34 @@ public class PageController {
         //test to see if files are properly received
         return "file-received";
     }
+
+    @GetMapping("/view")
+    public String view(Model model){
+        return "render";
+    }
+
+
+    @GetMapping("/render")
+    public ResponseEntity<Resource> render(Model model){
+
+
+        //getObject takes GetObjectRequest as an Argument
+        //we must provide GetObjectRequest with the bucket name and full key
+
+        ResponseInputStream<GetObjectResponse> stream =
+            s3client.getObject(
+                    GetObjectRequest.builder()
+                            .bucket("first-storage")
+                            .key("ThePalouse-1296-3200x2133.jpg")
+                    .build()
+            );
+
+        InputStreamResource resource = new InputStreamResource(stream);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(resource);
+    }
+
+
 
 }
